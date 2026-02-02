e40fb299cff3aa1d64b9767d5ef3216219fc6962
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2026-02-02 23:54:34 +0700

    Done session02 - Bai01.java

--- Session02/Bai01/Bai01.java
+++ Session02/Bai01/Bai01.java
@@ -1,27 +1,33 @@
 import java.util.Scanner;
 
 public class Bai01 {
+
+    // Hàm tính tổng từ 1 đến N
+    public static int sum (int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        // 1. Khởi tạo đối tượng Scanner để nhận dữ liệu từ bàn phím
+
+        // Tạo scanner
         Scanner scanner = new Scanner(System.in);
 
-        // 2. Yêu cầu người dùng nhập N
-        System.out.print("Nhập vào một số nguyên dương N: ");
+        System.out.print("Nhập một số nguyên dương N: ");
         int n = scanner.nextInt();
 
-        // 3. Kiểm tra điều kiện đầu vào (Validate input)
-        // Yêu cầu: Nếu số âm hoặc bằng 0 thì báo lỗi
+        // Kiểm tra Nếu người dùng nhập số không hợp lệ (ví dụ: số âm hoặc số 0), hiển thị thông báo lỗi “Số nhập vào không hợp lệ”
         if (n <= 0) {
-            System.out.println("Số nhập vào không hợp lệ");
+            System.out.print("Số nhập vào không hợp lệ");
         } else {
-            // 4. Xử lý tính tổng dùng vòng lặp for
-            long sum = 0; // Sử dụng long để tránh tràn số nếu tổng quá lớn
-            for (int i = 1; i <= n; i++) {
-                sum += i; // Cộng dồn giá trị i vào biến sum
-            }
+            // Xử lý
+            int sum = sum(n);
 
-            // 5. Hiển thị kết quả ra màn hình
-            System.out.println("Tổng các số từ 1 đến " + n + " là: " + sum);
+            // In kết quả ra màn hình
+            System.out.println("Tổng các số từ 1 đến "+ n + " là: " + sum);
         }
 
         // Đóng scanner
